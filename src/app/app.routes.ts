b099b8bc3aa2bf183c3da79eb0def7792290830e
Le Nguyen <97854609+lenguyen2022@users.noreply.github.com>
2024-04-11 08:44:53 -0400

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {BookComponent} from "./book/book.component";
+import {BookDetailsComponent} from "./book/book-details/book-details.component";
+import {EditBookComponent} from "./book/edit-book/edit-book.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'book', component: BookComponent, children: [
+      { path: ':id', component: BookDetailsComponent },
+      { path: ':id/edit', component: EditBookComponent }
+    ] },
+
+
+];
